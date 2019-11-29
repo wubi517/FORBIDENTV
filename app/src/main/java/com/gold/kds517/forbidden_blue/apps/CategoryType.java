6f1fbea4c6771093f862331896f8ae3ee06da627
yejong2013 <yejong2013@gmail.com>
2019-11-29 17:23:02 +0800

    Initial commit

---
+++ app/src/main/java/com/gold/kds517/forbidden_blue/apps/CategoryType.java
@@ -0,0 +1,5 @@
+package com.gold.kds517.forbidden_blue.apps;
+
+public enum CategoryType {
+        live,vod,series
+    }
---
+++ app/src/main/jni/notifications.c
@@ -0,0 +1,27 @@
+#include <jni.h>
+
+//Links from MainActivity
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getZero(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "Zm9yYg==");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getOne(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "Zm9yYm0y");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getTwo(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "cmVkeDI=");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getThree(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "ovL3RoZXJhZGlvc2hhay5jby51aMWF5OXBjSFIyWDNBeEwybHVaR2FIUjBjRG92TDNSb1pYSmhaR2x2YzJoaGF5NWpieTUxYXk5cGNIUjJYM0F4TDJsdVpHVjRMbkJvY0Q5c2FXTmxibU5sWDJ0bGVUMXNhWFpsZEhZeQ==");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getFour(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "aHR0cDovL3RoZXJhZGlvc2YXk1amJ5NTFheTlwY0hSMlgzQXlMYUhSMGNEb3ZMM1JvWlhKaFpHbHZjMmhoYXk1amJ5NTFheTlwY0hSMlgzQXlMMmx1WkdWNExuQm9jRDlzYVdObGJtTmxYMnRsZVQxc2FYWmxkSFl5");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getFive(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "http://theradioshak.co.uk/forb/icon.png");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getSix(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "http://theradioshak.co.uk/forbm2/icon.png");
+}
+JNIEXPORT jstring JNICALL Java_com_gold_kds517_forbidden_1blue_MainActivity_getSeven(JNIEnv *env, jobject instance){
+    return (*env)->NewStringUTF(env, "http://theradioshak.co.uk/redx2/icon.png");
+}
